deed59438e377692ddb3c551dc04303cbd7a21b5
Nicholas iyamabo <niyamabo@gmail.com>
2022-11-24 23:25:41 +0000

    a few notes for motion model

--- scripts/robot.py
+++ scripts/robot.py
@@ -6,3 +6,7 @@ class Robot(object):
         self.state = state
         self.location = location
         self.assignmentPoint = assignmentPoint
+
+        # add motion model subscribeers here stuff here TODO
+        # add functions to execute the actions here TODO
+
